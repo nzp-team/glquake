05d82bc901040f9316ef5cd0958d37b53960d87d
Ryan Baldwin <ryan.baldwin2011@gmail.com>
2022-08-19 21:42:57 -0700

    Add sprinting and other binddt functions

--- source/keys.h
+++ source/keys.h
@@ -124,6 +124,7 @@ typedef enum {key_game, key_console, key_message, key_menu, key_menu_pause} keyd
 
 extern keydest_t	key_dest;
 extern char *keybindings[256];
+extern char *dtbindings[256];
 extern	int		key_repeats[256];
 extern	int		key_count;			// incremented every key event
 extern	int		key_lastpress;
@@ -132,4 +133,5 @@ void Key_Event (int key, qboolean down);
 void Key_Init (void);
 void Key_WriteBindings (FILE *f);
 void Key_SetBinding (int keynum, char *binding);
+void Key_WriteDTBindings (FILE *f);
 void Key_ClearStates (void);
