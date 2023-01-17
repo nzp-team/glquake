d6599e343af48eee13467cabcc5e7e019f1c0ad0
Steam Deck User <motolegacy.git@gmail.com>
2023-01-16 20:16:56 -0500

    Rename Zombie Bodies to use % instead of #

--- source/gl_rmain.c
+++ source/gl_rmain.c
@@ -824,7 +824,7 @@ void R_DrawAliasModel (entity_t *e)
 	//
 	// locate the proper data
 	//
-	if(doZHack && specChar == '#')
+	if(doZHack && specChar == '%')
 	{
 		if(clmodel->name[strlen(clmodel->name) - 6] == 'c')
 			paliashdr = (aliashdr_t *) Mod_Extradata(Mod_FindName("models/ai/zcfull.mdl"));
@@ -871,7 +871,7 @@ void R_DrawAliasModel (entity_t *e)
 		
 	}
 
-	if (specChar == '#')//Zombie body
+	if (specChar == '%')//Zombie body
 	{
 		switch(e->skinnum)
 		{
@@ -927,7 +927,7 @@ void R_DrawAliasModel (entity_t *e)
 
 	glPopMatrix ();
 
-	if (doZHack == 0 && specChar == '#')//if we're drawing zombie, also draw its limbs in one call
+	if (doZHack == 0 && specChar == '%')//if we're drawing zombie, also draw its limbs in one call
 	{
 		if(e->z_head)
 			R_DrawZombieLimb(e,1);
@@ -983,7 +983,7 @@ void R_DrawEntitiesOnList (void)
 			continue;
 		}
 		doZHack = 0;
-		if(specChar == '#')
+		if(specChar == '%')
 		{
 			if(zHackCount > 5 || ((currententity->z_head != 0) && (currententity->z_larm != 0) && (currententity->z_rarm != 0)))
 			{
